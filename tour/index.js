79c9337428cb12f94c8f8cbaff2dc486e485a353
aljaf966 <aljaf966@newschool.edu>
2021-03-30 22:18:25 -0400

    edits

--- tour/index.js
+++ tour/index.js
@@ -1,4 +1,41 @@
-let button = document.querySelector ('.button');
-button.addEventListener('click', function () {
-  button.classList.add('buttonclick');
-});
+// let button = document.querySelector ('.button');
+// button.addEventListener('click', function () {
+//  button.classList.add('buttonclick');
+//});
+
+// function normalImg(x) {
+//  x.style.height = "500px";
+
+
+function revealMessage() {
+  document.getElementById("hiddenMessege").style.filter= 'blur(0px)';
+}
+
+
+function changeOpacity() {
+  document.getElementById("headertwo").style.opacity='100%';
+}
+
+function continueStory1() {
+  document.getElementbyId("revealMore1").style.filter='blur(0px)';
+}
+
+function continueStory2() {
+  document.getElementsByClassName("revealMore2").style.filter='blur(0px)';
+
+}
+
+function revealMessege2() {
+  document.getElementById('gardenDoor').style.display='block';
+
+}
+
+
+//function () {
+  //document.getElementByClassName("button1").style.height= "550px";
+//  document.getElementByClassName("button1").style.width = "550px";
+
+//function buttonSize2() {
+  //document.getElementByClassName("button2").style.height= "550px";
+  //  document.getElementByClassName("button2").style.width = "550px";
+// }
